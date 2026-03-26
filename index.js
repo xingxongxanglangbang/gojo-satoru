9738dde32cd92305ba1d2e28bd5479642a944f8b
xingxongxanglangbang <kauan.fernandes.santos04@escola.pr.gov.br>
2026-03-26 11:21:43 -0300

    Adicionei comentarios extras e personalidade

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-// run `node index.js` in the terminal
-
-console.log(`Hello Node.js v${process.versions.node}!`);
+//Números que serão usados para a soma e subtração
+let num1 = 8008;
+let num2 = 5808;
+// Soma do bagulho
+let soma = num1 + num2;
+//Subtração e tals
+let sub = num1 - num2;
+// Sá coisa aqui mostra a soma e subtração pro usuario.
+let mensagem = 'Doido tua soma deu {soma}, e tua subtração deu {sub}.'
+  .replace('{soma}', soma)
+  .replace('{sub}', sub);
+console.log(mensagem);
+// Condição da soma para saber se é maior q 5000 ou menor.
+let vSoma = soma >= 5000 ? 'Tua soma é maior que 5000 mano.' : 'Tua soma é menor que 5000 e tals.';
+console.log(vSoma);
+// Condição da subtração.
+let vSub = sub <= 0 ? 'A subtração é menor que 0' : 'A subtração é maior que 0';
+console.log(vSub);
