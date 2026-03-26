8b4b23db682dc9736e7b9e32b42f6eee36f58957
xingxongxanglangbang <kauan.fernandes.santos04@escola.pr.gov.br>
2026-03-26 11:44:52 -0300

    I added a multiplier

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ let num2 = 5808;
 let soma = num1 + num2;
 //Subtração e tals
 let sub = num1 - num2;
+//Multiplicação
+let mult = num1 * num2;
 // Sá coisa aqui mostra a soma e subtração pro usuario.
-let mensagem = 'Doido tua soma deu {soma}, e tua subtração deu {sub}.'
+let mensagem = 'Doido tua soma deu {soma}, tua subtração deu {sub} e tua multiplicação deu {mult}'
   .replace('{soma}', soma)
-  .replace('{sub}', sub);
+  .replace('{sub}', sub)
+  .replace('{mult}', mult);
 console.log(mensagem);
 // Condição da soma para saber se é maior q 5000 ou menor.
 let vSoma = soma >= 5000 ? 'Tua soma é maior que 5000 mano.' : 'Tua soma é menor que 5000 e tals.';
 console.log(vSoma);
 // Condição da subtração.
 let vSub = sub <= 0 ? 'A subtração é menor que 0' : 'A subtração é maior que 0';
-console.log(vSub);
+// Multiplicação
+let vMult = sub <= 1000000 ? 'Tua multiplicação é menor q 1000000' : 'A tua multiplicação é maior q 1000000'
+console.log(vMult);
